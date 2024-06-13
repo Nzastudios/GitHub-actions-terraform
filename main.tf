039d2828db8ae55b122bceb85fb1c4700fd0ae8e
nige-gcp-smartco <nigel.anthony@smart-co.co.uk>
2024-06-13 13:05:15 +0100

    updates made to wip

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "${var.dev-env}wbkt1"
+  name     = "testwbkt1"
   location = "europe-west2"
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "${var.dev-env}wbkt2"
+  name     = "testwbkt2"
   location = "europe-west2"
 }
