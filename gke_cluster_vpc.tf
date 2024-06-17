36b667750590ffac502a98fc5a4f9bda7af83590
nzastudios <creativedreamsx@icloud.com>
2024-06-17 19:30:43 +0100

    gke cluster vpc checkov fixes applied

--- gke_cluster_vpc.tf
+++ gke_cluster_vpc.tf
@@ -8,9 +8,24 @@ resource "google_compute_network" "vpc" {
 }
 
 # Subnet
+# https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-general-policies/ensure-gcp-subnet-has-a-private-ip-google-access
+# Enabling private IP Google access
 resource "google_compute_subnetwork" "subnet" {
-  name          = "checkov-dev-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  name                     = "checkov-dev-subnet"
+  region                   = var.region
+  network                  = google_compute_network.vpc.name
+  ip_cidr_range            = "10.10.0.0/24"
+  private_ip_google_access = true
+
+  # https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-networking-policies/ensure-gcp-private-google-access-is-enabled-for-ipv6
+  # Enabling Private Google Access for IPv6 can help improve the security
+  private_ipv6_google_access = "ENABLE_BIDIRECTIONAL_ACCESS_TO_GOOGLE"
+
+  # https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/logging-policies-1/bc-gcp-logging-1
+  # Enable VPC Flow logs for the subnet
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
