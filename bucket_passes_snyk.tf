f36bafe79b29c5889f607f0ad99d4612ed4f72a4
nzastudios <creativedreamsx@icloud.com>
2024-06-17 19:06:49 +0100

    testing bucket enforced

--- bucket_passes_snyk.tf
+++ bucket_passes_snyk.tf
@@ -8,17 +8,13 @@ resource "google_storage_bucket" "snykbucket" {
     enabled = true
   }
 
-  # Ensure public access prevention is enforced
-  # https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-general-policies/bc-google-cloud-114
-
-  # We want to protect this bucket. Terraform will not be able to deleted bucket force_destroy = true false
+  # We want to protect this bucket. Terraform will not be able to deleted bucket force_destroy = false
   # google_storage_bucket.foo: Error trying to delete a bucket containing objects without `force_destroy` set to true
   # https://github.com/hashicorp/terraform-provider-google/issues/1509
 
   force_destroy = true
 
-
-  # public_access_prevention = "enforced"
+  public_access_prevention = "enforced"
 
   uniform_bucket_level_access = true
 
