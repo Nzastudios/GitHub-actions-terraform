b275d02a8c18f793da55dc597082cded7e5a6892
nzastudios <creativedreamsx@icloud.com>
2024-06-17 19:43:51 +0100

    firewall added

--- gke_firewall.tf
+++ gke_firewall.tf
@@ -1,3 +1,5 @@
+# https://docs.prismacloud.io/en/enterprise-edition/policy-reference/google-cloud-policies/google-cloud-networking-policies/bc-gcp-2-18
+
 resource "google_compute_firewall" "testfw" {
   name    = "test-firewall"
   network = google_compute_network.vpc.self_link
